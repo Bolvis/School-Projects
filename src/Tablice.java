ce33cb3a50ca002738109ce2dbcd9d73e8e30748
Bolvis <dawid.stasiak17@gmail.com>
2019-10-09 15:43:00 +0200

    Initial commit

---
+++ src/Tablice.java
@@ -0,0 +1,31 @@
+import java.util.Random;
+
+public class Tablice {
+
+    protected static int[][] arrayOfInts;
+    protected static int sizeX,sizeY;
+    protected static long[] arrayOfSums;
+
+    public static void main(String[] args) {
+
+        try{
+        sizeX=Integer.valueOf(args[0]);
+        sizeY=Integer.valueOf(args[1]);
+        }
+        catch(Exception ex){System.out.println("It doesn't work"); System.exit(1);}
+        arrayOfInts= new int[sizeX][sizeY];
+        arrayOfSums = new long [sizeX];
+        for(int x=0;x<arrayOfInts.length;x++)
+        {
+            for(int y=0;y<arrayOfInts[x].length;y++)
+            {
+                Random r = new Random();
+                arrayOfInts[x][y]=r.nextInt();
+                arrayOfSums[x]+=arrayOfInts[x][y];
+                System.out.print(arrayOfInts[x][y]+",");
+            }
+            System.out.print("\n");
+            System.out.print("Sum is:"+arrayOfSums[x]+"\n");
+        }
+    }
+}
