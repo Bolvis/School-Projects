d7f85a5b8b790b893cba72ca841f57d9014cc473
Bolvis <dawid.stasiak17@gmail.com>
2019-11-20 14:27:35 +0100

    1

--- src/Person.kt
+++
@@ -1,7 +0,0 @@
-class Person constructor(val name:String, val surname:String, val age:String)
-{
-
-    override fun toString(): String {
-        return "[Name: "+this.name+" Surname: "+this.surname+" Age: "+this.age+"]"
-    }
-}
